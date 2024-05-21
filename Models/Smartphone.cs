2098efe2fe8402900cf35e7f1d5e2891453a1dd6
alexandrefreitass <xandy4321@hotmail.com>
2024-05-20 23:53:43 -0300

    projeto completo

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,37 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        // Campos privados
+        private string modelo;
+        private string imei;
+        private int memoria;
+
+        // Propriedades públicas para acessar os campos privados
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+
+        public string IMEI
+        {
+            get { return imei; }
+            set { imei = value; }
+        }
+
+        public int Memoria
+        {
+            get { return memoria; }
+            set { memoria = value; }
+        }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia X", imei: "111111111111111", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Whatsapp");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone iPhone:");
+        Smartphone iphone = new Iphone(numero: "123456", modelo: "iPhone 12", imei: "222222222222222", memoria: 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
